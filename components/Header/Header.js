d51e861ef214f08938e8fb9c4a5ebc2159d334fa
Marlon Clay <luciousmc@icloud.com>
2021-10-20 14:57:56 -0700

    adds tailwindcss/forms plugin and styles search bar

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,11 +1,19 @@
 import Image from 'next/image';
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from '@heroicons/react/outline';
 
 function Header() {
   return (
     <header>
       <div className='flex justify-between bg-white max-w-6xl'>
         {/* Left */}
-        <div className='relative hidden w-24 lg:inline-grid h- cursor-pointer'>
+        <div className='relative hidden w-24 lg:inline-grid cursor-pointer'>
           <Image
             src='https://links.papareact.com/ocw'
             layout='fill'
@@ -13,14 +21,25 @@ function Header() {
           />
         </div>
 
-        <div className='relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer'>
+        <div className='relative w-10 lg:hidden flex-shrink-0 cursor-pointer'>
           <Image
             src='https://links.papareact.com/jjm'
             layout='fill'
             objectFit='contain'
           />
         </div>
+
         {/* Middle */}
+        <div className='mt-1 relative p-3 rounded-md'>
+          <div className='absolute inset-y-0 pl-3 flex items-center pointer-events-none'>
+            <SearchIcon className='h-5 text-gray-400' />
+          </div>
+          <input
+            type='text'
+            placeholder='Search'
+            className='bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md focus:ring-black focus:border-black'
+          />
+        </div>
         {/* Right */}
       </div>
     </header>
